10ebfc4f0a7849e412bffdc55c19f4f056084da0
therealsamrajya <samrajyadai@gmail.com>
2023-09-20 22:09:15 +0545

    new

---
+++ one/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (<footer>
+    <div>
+      <h1>TECHDOLOR</h1>
+      <p>@all right reserved</p>
+    </div>
+
+    <div>
+      <h5> Follow Us</h5>
+      <div>
+        <a href='https://www.instagram.com/_saammu/' target={'blank'}>Instagram</a>
+        <a href='https://github.com/therealsamrajya' target={'blank'}>Github</a>
+      </div>
+    </div>
+  </footer>
+  );
+};
+
+export default Footer;
