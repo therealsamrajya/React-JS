24247306c415cf402aa2901dc0cc027547503e3e
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-02-09 20:24:21 +0545

    TRY1

---
+++ four/src/components/Home.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import toast from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
+
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+
+  const img2 =
+  "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/w/9/h/9-aadi1290-9-aadi-black-original-imagpyhv7tz6wx6e.jpeg?q=70";
+
+
+
+const Home = () => {
+const productList=[
+  {
+    name:'Mac Book',
+    price:12000,
+    imgSrc:img1,
+    id:'asda',
+},
+{
+    name:'Black Shoes',
+    price:400,
+    imgSrc:img2,
+    id:'aseeda',
+},
+];
+const dispatch = useDispatch()
+
+const addToCartHandler = (options) => {
+  dispatch({type:"addToCart",payload:options})
+  toast.success('Added to Cart')
+ 
+};
+  return (
+    <div className='home'>
+     {
+      productList.map(i=>(
+        <ProductCard key={i.id}imgSrc={i.imgSrc} name={i.name}price={i.price}
+        id={i.id}
+        handler={addToCartHandler}
+        />
+      ))
+     }
+    </div>
+    
+  )
+}
+const ProductCard = ({name,id,price,handler,imgSrc}) =>(
+  <div className='productCard'>
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={()=>handler({name,price,id,quantity:1,imgSrc})}>Add to Cart</button>
+  </div>
+)
+
+export default Home
---
+++ four/src/redux/reducers.js
@@ -0,0 +1,54 @@
+import { createReducer, createAction } from "@reduxjs/toolkit";
+
+// Define action creators
+export const addToCart = createAction('addToCart');
+export const decrement = createAction('decrement');
+export const deleteFromCart = createAction('deleteFromCart');
+export const calculatePrice = createAction('calculatePrice');
+
+export const cartReducer = createReducer(
+  {
+    cartItems: [],
+    subTotal: 0,
+    shipping: 0,
+    tax: 0,
+    total: 0,
+  },
+  (builder) => {
+    builder
+      .addCase(addToCart, (state, action) => {
+        const item = action.payload;
+        const isItemExist = state.cartItems.find((i) => i.id === item.id);
+
+        if (isItemExist) {
+          state.cartItems.forEach((i) => {
+            if (i.id === item.id) i.quantity += 1;
+          });
+        } else {
+          state.cartItems.push({ ...item, quantity: 1 });
+        }
+      })
+      .addCase(decrement, (state, action) => {
+        const itemId = action.payload;
+        const item = state.cartItems.find((i) => i.id === itemId);
+        
+        if (item && item.quantity > 1) {
+          state.cartItems.forEach((i) => {
+            if (i.id === itemId) i.quantity -= 1;
+          });
+        }
+      })
+      .addCase(deleteFromCart, (state, action) => {
+        const itemId = action.payload;
+        state.cartItems = state.cartItems.filter((i) => i.id !== itemId);
+      })
+      .addCase(calculatePrice, (state) => {
+        let sum = 0;
+        state.cartItems.forEach((i) => (sum += i.price * i.quantity));
+        state.subTotal = sum;
+        state.shipping = state.subTotal > 1000 ? 0 : 200;
+        state.tax = +(state.subTotal * 0.18).toFixed();
+        state.total = state.subTotal + state.tax + state.shipping;
+      });
+  }
+);
