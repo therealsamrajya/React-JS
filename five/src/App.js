26b3ce2ee2bbe50dc5cfd68a419cbad730e0edce
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-02-11 08:52:41 +0545

    WorkSection

--- five/src/App.js
+++ five/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Home from "./components/Home"
+import Work from "./components/Work"
 
 function App() {
   return (
     <>
     <Header/>
     <Home />
+    <Work />
     </>
   );
 }
