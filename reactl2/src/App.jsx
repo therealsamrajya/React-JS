d34d0060cce130b51555bf8dc68fa5c2cd85ebed
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-02-21 21:15:25 +0545

    converse navbar  not yet responsive

---
+++ reactl2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar';
+import './App.css'
+import Card from './components/Card';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  )
+}
+
+export default App
