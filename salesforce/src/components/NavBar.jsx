03caee176931a4fcb5422f24e67e8c9cc06ec866
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-03-08 14:08:40 +0545

    salesforce navbar

--- salesforce/src/components/NavBar.jsx
+++ salesforce/src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaSalesforce, FaAngleDown } from "react-icons/fa";
-
+import { IoMdSearch } from "react-icons/io";
+import { RiEarthFill } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
 const menu = [
   {
     id: 1,
@@ -32,6 +34,7 @@ const menu = [
 
 const NavBar = () => {
   const [showSubMenu, setShowSubMenu] = useState(false);
+  const [showSubMenu1, setShowSubMenu1] = useState(false);
 
   const handleMouseEnter = () => {
     setShowSubMenu(true);
@@ -41,9 +44,17 @@ const NavBar = () => {
     setShowSubMenu(false);
   };
 
+  const handleMouseEnter1 = () => {
+    setShowSubMenu1(true);
+  };
+
+  const handleMouseLeave1 = () => {
+    setShowSubMenu1(false);
+  };
+
   return (
     <div className="">
-      <div className="flex flex-row items-center w-3/6 justify-between ml-[-3%] mt-[-2%] space-x-10">
+      <div className="flex flex-row items-center w-3/6 justify-between ml-[-3%] mt-[-2%] space-x-8">
         <NavLink to="/">
           <FaSalesforce className="w-[12vh] h-[12vh]" />
           <span className="hidden">Home </span>
@@ -79,6 +90,67 @@ const NavBar = () => {
               </NavLink>
             </div>
           )}
+        </div>
+        <div className="font-sans flex flex-col items-center  ">
+          <a
+            href="#"
+            className="text-md underline truncate px-[13vh]  text-secondary">
+            Contact Us
+          </a>
+          <h3 className="text-primary text-md font-sans">
+            +65 6302 5700 (Intl)
+          </h3>
+        </div>
+        <div>
+          <IoMdSearch className="w-6 h-6" />
+        </div>
+        <div>
+          <RiEarthFill className="w-6 h-6 " />
+        </div>
+        <div
+          className="relative"
+          onMouseEnter={handleMouseEnter1}
+          onMouseLeave={handleMouseLeave1}>
+          <button className="text-primary font-sans font-semibold hover:text-secondary focus:outline-none flex items-center">
+            <FaUser className="ml-1" />
+            Login
+          </button>
+          {showSubMenu1 && (
+            <div className="absolute top-full left-[-120%] bg-white w-[45vh] h-[55vh] p-2 rounded-lg  text-primary font-sans font-semibold shadow-2xl">
+              <h3 className="block text-gray-700 hover:text-primary text-left  ml-3 mt-3 font-bold text-xl">
+                Login
+              </h3>
+              <h3 className="block text-gray-700 hover:text-primary text-left  ml-3 mt-3">
+                Products
+              </h3>
+              <NavLink
+                to="/company"
+                className="block text-gray-700 hover:text-primary mb-2 text-left py-2 ml-3 mt-3 underline">
+                Salesforce
+              </NavLink>
+              <NavLink
+                to="/salesforce"
+                className="block text-gray-700 hover:text-primary text-left ml-3 underline">
+                Marketing Cloud
+              </NavLink>
+              <hr className="mt-2" />
+              <h3 className="text-left mt-4 ml-3 ">Experiences</h3>
+              <NavLink
+                to="/salesforce"
+                className="block text-gray-700 hover:text-primary mt-2 text-left ml-3 underline">
+                Trailblazer account
+              </NavLink>
+              <p className="ml-4 text-left mt-2  text-sm tracking-tight ">
+                Access Trailhead, your Trailblazer profile, community, learning,
+                original series, events, support, and more.
+              </p>
+            </div>
+          )}
+        </div>
+        <div class="flex">
+          <button class="bg-green-700 hover:bg-green-950 text-white font-bold py-1 px-2 whitespace-nowrap rounded w-[20vh] h-[7vh]">
+            Try for Free
+          </button>
         </div>
       </div>
     </div>
